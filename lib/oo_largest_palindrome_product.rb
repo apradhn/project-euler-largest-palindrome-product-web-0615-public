7b5aef339f1ba6760157682a0cd8ac4d9b506df1
apradhn <abhijit.pradhan6@gmail.com>
2015-07-07 15:51:21 -0400

    refactored to speed up solution

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,32 +1,24 @@
 class LargestPalindromeProduct
 
   def answer
-    first = 100 * 100
-    last = 999 * 999
-    palindromes = (first..last).select { |num| palindrome?(num) }
-    palindromes.reverse.find{|palindrome| three_digit_product?(palindrome)}    
+    # Your objective is to find the largest palindrome made from the product of two three-digit numbers.
+    num = 999**2
+    while num > 0 do 
+      if palindrome?(num) && has_three_digit_factors?(num)
+        break
+      else 
+        num -= 1
+      end
+    end
+    num
+  end
+
+  def has_three_digit_factors?(num)
+    (100..999).any? {|n| num % n == 0 && (num / n) < 1000 }
   end
 
   def palindrome?(num)
     num.to_s == num.to_s.reverse
   end
 
-  def factors_of(num)
-    (1..num).select {|n| n if num % n == 0 }
-  end
-
-  def three_digit_product?(num)
-    factors = factors_of(num)
-    three_digit_factors = three_digit_numbers_of(factors)
-    # is there any three digit factor that equals num when multiplied by any other three digit factor?
-    three_digit_factors.collect{|factor| three_digit_factors.collect{|f| factor * f}}.flatten.include?(num)
-  end
-
-  def three_digit_numbers_of(numbers)
-    numbers.select{|num| num.to_s.length == 3}
-  end
-
-  def three_digits?(num)
-    num.to_s.length === 3
-  end  
 end
